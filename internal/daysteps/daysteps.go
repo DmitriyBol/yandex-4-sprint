a8730ab535be200217c5aa83442a56abcb61b1f9
Dmitriy B <72795383+DmitriyBol@users.noreply.github.com>
2025-12-03 19:26:30 +0400

    feat: added core functionality

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,14 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -12,9 +19,43 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	params := strings.Split(data, ",")
+
+	if len(params) != 2 {
+		return 0, 0, errors.New("invalid data params in :parsePackage:daysteps")
+	}
+
+	steps, err := strconv.Atoi(params[0])
+	if err != nil {
+		return 0, 0, errors.New("unable to convert steps to integer in :parsePackage")
+	}
+	if steps <= 0 {
+		return 0, 0, errors.New("steps cannot be 0 in :parsePackage")
+	}
+
+	duration, err := time.ParseDuration(params[1])
+	if err != nil {
+		return 0, 0, errors.New("unable to convert duration to time in :parsePackage")
+	}
+	if duration <= 0 {
+		return 0, 0, errors.New("duration must be positive in :parsePackage")
+	}
+
+	return steps, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	distance := float64(steps) * stepLength / mInKm
+	calories, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+	if err != nil {
+		return "error to calculate calories in :DayActionInfo"
+	}
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distance, calories)
 }
